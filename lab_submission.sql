fa07a6847f8e154fe1e93ecd8acf1c3388605457
V3en-us <venus.kabera@strathmore.edu>
2024-11-08 22:12:43 +0300

    done

--- lab_submission.sql
+++ lab_submission.sql
@@ -27,7 +27,7 @@ DELIMITER ;
 
 DELIMITER $$
 
-CREATE FUNCTION `FUNC_VEN`(orderAmount DOUBLE, disRate DOUBLE) RETURNS DOUBLE
+CREATE FUNCTION `FUNC_LAB5`(orderAmount DOUBLE, disRate DOUBLE) RETURNS DOUBLE
 DETERMINISTIC
 BEGIN
     DECLARE discAmount DOUBLE;
@@ -37,3 +37,21 @@ END$$
 
 DELIMITER ;
 -- (iii) A View called VIEW_LAB5
+CREATE VIEW `VIEW_LAB5` AS
+SELECT
+    customers.customerNumber AS 'Customer ID',
+    customers.customerName AS 'Customer Name',
+    orders.orderNumber AS 'Order No.',
+    orders.orderDate AS 'Order Date',
+    orders.amount AS 'Order Amnt',
+    FUNC_LAB5(orders.amount, customers.discRate) AS 'Discount Amount'
+FROM
+    customers
+JOIN
+    orders ON customers.customerNumber = orders.customerNumber
+WHERE
+    orders.orderDate = (
+        SELECT MAX(orderDate)
+        FROM orders AS latestOrders
+        WHERE latestOrders.customerNumber = customers.customerNumber
+    );
